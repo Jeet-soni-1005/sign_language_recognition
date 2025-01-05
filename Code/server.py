bb411fe9edf751b478aed1ebfb147e247fb91b79
Jeet Soni <137893736+Jeet-soni-1005@users.noreply.github.com>
2025-01-05 13:44:24 +0530

    Update server.py

--- Code/server.py
+++ Code/server.py
@@ -1,31 +1,29 @@
-from flask import Flask, request
-# import pandas as pd
+from flask import Flask, request, jsonify
 import numpy as np
 
+app = Flask(__name__)
 
 all_data = []
 i = 0
 
-app = Flask(__name__)
-
+# POST endpoint to receive data
 @app.route('/feed', methods=['POST'])
-def get_data():
-    data = request.data.decode('ascii')
-    print(data)
-    return 'OK'
+def feed_data():
+    data = request.get_json()  # Expecting JSON payload
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+    all_data.append(data)
+    global i
+    i += 1
+    if i == 10:
+        np.save('data.npy', np.array(all_data).astype(np.float32))
+        i = 0
+    return jsonify({"message": "Data received successfully"}), 200
 
-# @app.route('/feed', methods=['POST'])
-# def get_data():
-#     global all_data, i
-#     i += 1
-#     data = request.data.decode('ascii')
-#     # print(request.data)
-#     all_data.append(data.split(","))
-#     if i == 10:
-#         np.save('data.npy', np.array(all_data).astype(np.float32))
-#         print("Saved")
-#         i = 0
-#     return 'OK'
+# GET endpoint to retrieve data
+@app.route('/data', methods=['GET'])
+def get_data():
+    return jsonify(all_data), 200
 
 if __name__ == '__main__':
     app.run(host='192.168.201.151', port=5000, debug=True)
